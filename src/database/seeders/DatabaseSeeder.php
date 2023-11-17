e72c4aea3e62dbc522f9d2a8dc9e33fcac8a25bf
Franver S de Moraes <franver@gmail.com>
2023-11-17 10:04:55 -0300

    Ajuste nos seeder, para criar o primeiro usuário.

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -7,6 +7,7 @@ use App\Models\Book;
 use App\Models\Heap;
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,13 +16,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory(10)->create();
+//        User::factory(10)->create();
         Heap::factory(35)->create();
         Book::factory(50)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+         User::factory()->create([
+             'name' => 'Test User',
+             'email' => 'test@example.com',
+             'password' => Hash::make('password'),
+         ]);
     }
 }
